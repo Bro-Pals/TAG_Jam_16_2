8bcc916a50275d9ba4e7fd21f3c0329f7fa6bea9
Kevin <kprehn01@gmail.com>
2014-11-08 21:56:39 -0600

    DOing more cool ironclad stuff

--- src/bropals/tag16_2/projectile/Projectile.java
+++ src/bropals/tag16_2/projectile/Projectile.java
@@ -1,17 +1,21 @@
 package bropals.tag16_2.projectile;
 
+import java.util.ArrayList;
+
 public class Projectile {
 	
 	private float x, y, angle, speed;
+	private boolean enemyProjectile; // is this an enemy projectile?
 	
-	public Projectile(float x, float y, float angle, float speed) {
+	public Projectile(float x, float y, float angle, float speed, boolean enemyProjectile) {
 		this.x=x;
 		this.y=y;
 		this.angle=angle;
 		this.speed=speed;
+		this.enemyProjectile=enemyProjectile;
 	}
 	
-	public void update() {
+	public void update(ArrayList<BaseCreature> enemies, BaseCreature ironclad, ArrayList<Projectile> projectiles) {
 		this.x+=Math.cos(angle);
 		this.y+=Math.sin(angle);
 	}
