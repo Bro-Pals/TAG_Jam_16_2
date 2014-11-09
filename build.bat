42b2e53f835a20f66745b42934cead52f0a08070
RadioLemon <horsentp@gmail.com>
2014-11-08 21:57:34 -0600

    Updated the build file

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-javac -d class src/bropals/tag16_2/*.java src/bropals/tag16_2/creature/*.java
+javac -d class src/bropals/tag16_2/*.java src/bropals/tag16_2/creature/*.java src/bropals/tag16_2/projectile/*.java
 PAUSE
 jar cfm IroncladGame.jar MANIFEST.mf -C class .
